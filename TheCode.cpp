56da61ca13eeb43a603b09a1f1ee6ee7f1613fe0
Rishthegreat <rishitarora2005@gmail.com>
2021-01-30 20:22:02 -0500

    Add

--- TheCode.cpp
+++ TheCode.cpp
@@ -2,5 +2,9 @@
 
 int main()
 {
+    int players;
+    int money = 1500;
     std::cout << "Hello, this is our code and I don't know what it does!\n";
+    std::cout << "How many users: "; 
+    std::cin >> players;
 }
